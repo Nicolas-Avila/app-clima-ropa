96e90d6174a60e60a8b432d1b357d14090403195
Nicolas <aviladanielnicolas@gmail.com>
2025-06-11 20:21:17 -0300

    texto modificado WIP

--- back/api/IAmodel.js
+++ back/api/IAmodel.js
@@ -16,7 +16,7 @@ export async function clothingTemperature() {
 
   conversationHistory.push({
     role: "user",
-    content: `${temperatura} quiero que me digas una lista de la ropa que podría usar en la cabeza, en la parte superior, en la parte inferior y en los pies, de manera coloquial argentina.`,
+    content: `${temperatura} quiero que me digas una lista de la ropa que podría usar en la cabeza, en la parte superior, en la parte inferior y en los pies, de manera coloquial argentina. pero quiero que me dividas todo en un json para yo poder usarlo en una pagina web`,
   });
 
   try {
